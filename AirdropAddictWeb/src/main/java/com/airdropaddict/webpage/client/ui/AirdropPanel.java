2e815eb713eae193c28397f6d1f245a8193a72d9
Dzn <dzngit@gmail.com>
2017-12-17 23:24:46 +0100

    - Client

--- AirdropAddictWeb/src/main/java/com/airdropaddict/webpage/client/ui/AirdropPanel.java
+++ AirdropAddictWeb/src/main/java/com/airdropaddict/webpage/client/ui/AirdropPanel.java
@@ -1,18 +1,36 @@
 package com.airdropaddict.webpage.client.ui;
 
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Widget;
 
 public class AirdropPanel extends HTML {
 	private HTML panel;
 	private HTML header;
 	private HTML body;
 	private HTML footer;
-	
-	public AirdropPanel() {
-		super("<div class=\"col-lg-4\"></div>");
-		panel = new HTML("<div class=\"panel panel-primary\"></div>");
-		header = new HTML("<div class=\"panel-heading\"></div>");
-		body = new HTML("<div class=\"panel-body\"></div>");
-		footer = new HTML("<div class=\"panel-footer\"></div>");
+
+	public AirdropPanel(Widget parent) {
+		super();
+		setStyleName("col-lg-4");
+
+		panel = new HTML();
+		panel.setStyleName("panel");
+		panel.addStyleName("panel-primary");
+		DOM.appendChild(getElement(), panel.getElement());
+
+		header = new HTML();
+		header.setStyleName("panel-heading");
+		DOM.appendChild(panel.getElement(), header.getElement());
+
+		body = new HTML();
+		body.setStyleName("panel-body");
+		DOM.appendChild(panel.getElement(), body.getElement());
+
+		footer = new HTML();
+		footer.setStyleName("panel-footer");
+		DOM.appendChild(panel.getElement(), footer.getElement());
+
+		DOM.appendChild(parent.getElement(), getElement());
 	}
 }
