1bee28b463adc51a94b493962843ae2344b2c3e8
Danijel Zecevic <danijel.zecevic@marand.si>
2017-12-21 15:07:44 +0100

    - Client...

---
+++ AirdropAddictWeb/src/main/java/com/airdropaddict/webpage/client/ui/UntrustedAirdropsContainer.java
@@ -0,0 +1,18 @@
+package com.airdropaddict.webpage.client.ui;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+public class UntrustedAirdropsContainer extends Composite {
+	private static UntrustedAirdropsContainerUiBinder uiBinder = GWT.create(UntrustedAirdropsContainerUiBinder.class);
+
+	interface UntrustedAirdropsContainerUiBinder extends UiBinder<Widget, UntrustedAirdropsContainer> {
+	}
+
+	public UntrustedAirdropsContainer() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+}
