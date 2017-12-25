63419940611e9ab8f446ab143d58abe85c52ddf4
Dzn <dzngit@gmail.com>
2017-12-25 16:38:01 +0100

    - Client

--- AirdropAddictWeb/src/main/java/com/airdropaddict/webpage/client/ui/UntrustedAirdropsContainer.java
+++ AirdropAddictWeb/src/main/java/com/airdropaddict/webpage/client/ui/UntrustedAirdropsContainer.java
@@ -1,9 +1,14 @@
 package com.airdropaddict.webpage.client.ui;
 
+import com.airdropaddict.webpage.shared.data.UntrustedAirdropData;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+
+import gwt.material.design.client.ui.table.MaterialDataTable;
+import gwt.material.design.client.ui.table.cell.TextColumn;
 
 public class UntrustedAirdropsContainer extends Composite {
 	private static UntrustedAirdropsContainerUiBinder uiBinder = GWT.create(UntrustedAirdropsContainerUiBinder.class);
@@ -11,7 +16,31 @@ public class UntrustedAirdropsContainer extends Composite {
 	interface UntrustedAirdropsContainerUiBinder extends UiBinder<Widget, UntrustedAirdropsContainer> {
 	}
 
+	@UiField
+	MaterialDataTable<UntrustedAirdropData> table;
+
 	public UntrustedAirdropsContainer() {
 		initWidget(uiBinder.createAndBindUi(this));
+
+		table.addColumn(NameColumn.instance);
+		table.addColumn(UrlColumn.instance);
+	}
+
+	private static class NameColumn extends TextColumn<UntrustedAirdropData> {
+		private static final NameColumn instance = new NameColumn();
+
+		@Override
+		public String getValue(UntrustedAirdropData object) {
+			return object.name;
+		}
+	}
+
+	private static class UrlColumn extends TextColumn<UntrustedAirdropData> {
+		private static final UrlColumn instance = new UrlColumn();
+
+		@Override
+		public String getValue(UntrustedAirdropData object) {
+			return object.name;
+		}
 	}
 }
---
+++ AirdropAddictWeb/src/main/java/com/airdropaddict/webpage/shared/data/UntrustedAirdropData.java
@@ -0,0 +1,7 @@
+package com.airdropaddict.webpage.shared.data;
+
+public class UntrustedAirdropData extends EntityData {
+	public String name;
+	public String description;
+	public String url;
+}
